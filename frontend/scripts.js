c512da7f03dfbec80e8e3dd6d8ccb6535d687511
BenJewell <97567577+BenJewell@users.noreply.github.com>
2022-03-31 15:16:25 -0400

    scripting progress

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,7 +1,40 @@
+
+// Socket managment
 var socket = io();
-socket.emit('requestID')
 
 socket.on('ID', (ID) => {
     console.log('id: ' + ID)
     jQuery('#qrcode').qrcode(ID.toString());
 });
+
+// New session preparations
+function initiate(type) {
+    if (type == "phone") {
+
+    }
+    else if (type == "pc") {
+        socket.emit('requestID')
+    }
+}
+
+// Phone page element managment
+function setupPage(mode) {
+    if (mode == "camera") {
+        console.log("setting up page for camera")
+        document.getElementById("qr-reader").style.display = "block";
+        document.getElementById("code-box").style.display = "none";
+    }
+    else if (mode == "manual") {
+        console.log("setting up page for code")
+        document.getElementById("qr-reader").style.display = "none";
+        document.getElementById("code-box").style.display = "block";
+    }
+  }
+
+// QR scanner
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
+}
+var qrScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: 250 });
+    qrScanner.render(onScanSuccess);
