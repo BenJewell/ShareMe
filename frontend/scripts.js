b08a97ef6df4dc36ff7b9ea37277d83b01ff827c
BenJewell <97567577+BenJewell@users.noreply.github.com>
2022-04-15 18:56:46 -0400

    Swapped lib file from node version. Fixed camera and toggle bug which took like 3 hours.

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -15,16 +15,16 @@ function initiate(type) {
             //console.log(`Code scanned = ${decodedText}`, decodedResult);
             sendData(decodedText, document.getElementById('url').value)
         }
-        // var qrScanner = new Html5QrcodeScanner(
-        //     "qr-reader", { fps: 10, qrbox: 350 });
-        // qrScanner.clear()
-        // qrScanner.render(onScanSuccess);
-
-        var qrScanner = new Html5Qrcode("qr-reader");
-        const config = { fps: 2 };
-        qrScanner.start({ facingMode: "environment" }, config, onScanSuccess);
+        var qrScanner = new Html5QrcodeScanner(
+            "qr-reader", { fps: 10, qrbox: 350 });
+        //qrScanner.clear()
         qrScanner.render(onScanSuccess);
-        alert(qrScanner.getState())
+
+        // var qrScanner = new Html5Qrcode("qr-reader");
+        // const config = { fps: 2 };
+        // qrScanner.start({ facingMode: "environment" }, config, onScanSuccess);
+        // qrScanner.render(onScanSuccess);
+        // alert(qrScanner.getState())
 
     }
     else if (type === "pc") {
@@ -39,15 +39,21 @@ function initiate(type) {
 
 // Phone page element managment
 function setupPage(mode) {
+    // We need to use the visible property to avoid breaking the camera, but we use display
+    // property on buttons so they align properly when toggled.
     if (mode === "camera") {
         console.log("setting up page for camera")
-        document.getElementById("qr-reader").style.display = "block";
+        document.getElementById("qr-reader").style.visibility = "visible";
         document.getElementById("code-box").style.display = "none";
+        document.getElementById("manual-code").style.display = "block";
+        document.getElementById("start-camera").style.display = "none";
     }
     else if (mode === "manual") {
         console.log("setting up page for code")
-        document.getElementById("qr-reader").style.display = "none";
-        document.getElementById("code-box").style.display = "block";
+        document.getElementById("qr-reader").style.visibility = "hidden";
+        document.getElementById("code-box").style.display = "inline-block";
+        document.getElementById("manual-code").style.display = "none";
+        document.getElementById("start-camera").style.display = "block";
     }
 }
 
